44cfa563c0deafd6e316dcf89d6cf10323724c06
MAOZ <maooz4426@gmail.com>
2024-10-11 11:38:24 +0900

    first

---
+++ config/key.lua
@@ -0,0 +1,36 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+return {
+-- ショートカットキー設定
+keys = {
+	-- カーソルを一単語後ろに移動
+  {
+    key = "LeftArrow",
+    mods = "CMD",
+    action = act.SendKey {
+      key = "b",
+      mods = "META",
+    },
+  },
+  -- カーソルを一単語前に移動
+  {
+    key = "RightArrow",
+    mods = "CMD",
+    action = act.SendKey {
+      key = "f",
+      mods = "META",
+    },
+  },
+-- カーソルを一単語削除
+  {
+    key = "Backspace",
+    mods = "CMD",--mac用
+    action = act.SendKey {
+      key = "w",
+      mods = "CTRL",
+    },
+  },
+}
+}
+
