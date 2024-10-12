db59e7340c6224ca0d165097003c84d0a803ea7e
MAOZ <maooz4426@gmail.com>
2024-10-12 11:28:13 +0900

    タブの分割機能ショートカットを追加

--- config/key.lua
+++ config/key.lua
@@ -5,6 +5,21 @@ local config = {}
 
 -- ショートカットキー設定
 config.keys = {
+  --　タブを分割
+  {
+    key = '|',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitPane {
+      direction = 'Right',
+      size = { Percent = 50 },
+    },
+  },
+  -- タブを閉じる
+  {
+    key = 'w',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
 	-- カーソルを一単語後ろに移動
   {
     key = "LeftArrow",
