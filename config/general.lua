0e127e8b1bec0747f3c38856c3131b9dd28f80de
MAOZ <maooz4426@gmail.com>
2024-10-12 11:19:25 +0900

    wezterm閉じていいかの確認を取るように変更

--- config/general.lua
+++ config/general.lua
@@ -2,8 +2,7 @@ local config = {}
 
 -- 基本設定
 config.automatically_reload_config = true
-config.window_close_confirmation = "NeverPrompt"
-config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
 config.default_cursor_style = "BlinkingBar"
 
 return config
